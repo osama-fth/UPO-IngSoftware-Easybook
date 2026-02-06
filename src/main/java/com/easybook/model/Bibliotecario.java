185225b9d1572853bb4552924b2af36626722116
Osama Foutih <20054809@studenti.uniupo.it>
2026-02-06 10:43:23 +0100

    aggiunta documento implementazione

--- src/main/java/com/easybook/model/Bibliotecario.java
+++ src/main/java/com/easybook/model/Bibliotecario.java
@@ -7,33 +7,19 @@ package com.easybook.model;
  * @author Riccardo Negrini 20054675
  */
 public class Bibliotecario {
-    private String matricola;
-    private String nome;
-    private String cognome;
-    private String username;
-    private String password;
+    private final String nome;
+    private final String cognome;
 
     public Bibliotecario(String matricola, String nome, String cognome, String username, String password) {
-        this.matricola = matricola;
         this.nome = nome;
         this.cognome = cognome;
-        this.username = username;
-        this.password = password;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public String getCognome() {
         return cognome;
-    }
-
-    public void setCognome(String cognome) {
-        this.cognome = cognome;
     }
 }
